7ef7e93657f7322fa3b7db093c2ba529eafcdbd8
mperozo <perozo.marcos@gmail.com>
2020-04-17 19:24:33 -0300

    Adicionando CORS para acessar a API via client

--- consultorio/src/main/java/com/mperozo/consultorio/ConsultorioApplication.java
+++ consultorio/src/main/java/com/mperozo/consultorio/ConsultorioApplication.java
@@ -2,10 +2,20 @@ package com.mperozo.consultorio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ConsultorioApplication {
+@EnableWebMvc
+public class ConsultorioApplication implements WebMvcConfigurer {
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		//TODO colocar url específica
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConsultorioApplication.class, args);
 	}
